44dc7f29cd9b91f0383270b690dfa3c77574f5ef
jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2026-05-09 14:50:15 -0500

    jdjsdsjk

---
+++ ConsoleApp/Hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+public class Programm
+{
+    public static  void Main()
+    {
+        Console.WriteLine( "Hello World");
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// #Firts Messaage
+//Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Congratulations!");
+//Console.Write("You Wrote the first line of code");
+
+
+// Challenges 
+//Console.WriteLine("This is the first Line");
+//Console.WriteLine("This is the Second Line");
+//Console.Write("This is");
+//Console.Write(" ");
+//Console.Write("Thrid Line");
+
+
